3d0a5c3a46086e2ed72503d1672a108b06e3982e
tada-tada <120736501+tada-tada@users.noreply.github.com>
2025-01-04 18:14:36 +0900

    ver 0.9.5
    ver 0.9.5

---
+++ Extension095/blackboard_action.js
@@ -0,0 +1,59 @@
+const ms = 1000;	//1000ms
+
+//course
+const courseInterval = setInterval(() => {
+	if("https://king-lms.kcg.edu/ultra/course" == location.href || "https://king-lms.kcg.edu/ultra/course/#" == location.href) {
+		const course = document.getElementsByClassName("course-title");
+		if(typeof course[0] != 'undefined' && course[0].children[0].id.split("-")[2] != "") {
+			saveCourse();
+		}
+	}
+}, ms);
+
+//task
+const deadlineInterval = setInterval(() => {
+	if("https://king-lms.kcg.edu/ultra/deadline" == location.href  || "https://king-lms.kcg.edu/ultra/deadline/#" == location.href) {		
+		const deadlineL = document.getElementsByClassName("element-details").length;
+		if(deadlineL != 0) {
+			saveTask();
+		}
+	}
+}, ms);
+
+//stream
+const streamInterval = setInterval(() => {
+	if("https://king-lms.kcg.edu/ultra/stream" == location.href  || "https://king-lms.kcg.edu/ultra/stream/#" == location.href) {	
+		if(document.getElementsByClassName("activity-feed").length === 2) {
+			const upcomingStream = document.getElementsByClassName("activity-feed")[0].children.length;
+			const previousStream = document.getElementsByClassName("activity-feed")[1].children.length;
+	
+			if(upcomingStream != 0 && previousStream != 0) {
+				streamTaskCourseSave();
+			}	
+		}
+	}
+}, ms);
+
+//outline
+let completBool = true;
+const outlineInterval = setInterval(() => {
+	//outline
+	let location_href = location.href;
+	if(location_href.split("/")[(location_href.split("/").length)-1] === "outline" || location_href.split("/")[(location_href.split("/").length)-2] === "outline") {	
+		if(document.getElementsByClassName("click-to-invoke-container").length !== 0) {
+			outline();
+		}
+	}
+
+	//complet
+	if(typeof document.getElementsByTagName("button")[52] !== "undefined") {
+		if(completBool) {
+			document.getElementsByTagName("button")[52].addEventListener('click', ()=>{
+				outline_complet();
+			})
+			completBool = false;
+		}
+	}else {
+		completBool = true;
+	}
+}, ms);
---
+++ Extension095/option_period.js
@@ -0,0 +1,17 @@
+let retentionPeriodButton = document.getElementById("retentionPeriodButton");
+retentionPeriodButton.addEventListener('click',() => {
+	let retentionPeriod = document.querySelector("#retentionPeriodTable > tbody > tr:nth-child(1) > td > select").value;
+
+	if(retentionPeriod == "1day") {
+		retentionPeriod = 1*60*60*24*1000;
+	}else if(retentionPeriod == "1week") {
+		retentionPeriod = 1*60*60*24*7*1000;
+	}else if(retentionPeriod == "1month") {
+		retentionPeriod = 1*60*60*24*30*1000;
+	}
+
+	const retentionPeriod_key = "retentionPeriod";
+	chrome.storage.local.set({ [retentionPeriod_key]: retentionPeriod });
+
+	alert("課題の保存期間を登録しました。");
+})
